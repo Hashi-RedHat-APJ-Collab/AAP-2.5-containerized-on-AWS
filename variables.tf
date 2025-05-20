a0ede38fc49a259838f046eed57bfa9bd0d8d7bf
Aaron Evans <aarone@outlook.com.au>
2025-05-20 20:06:15 +1000

    Add missing variables for Red Hat username and password in Terraform configuration

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   type    = string
   default = "ap-southeast-2"
 }
+
+variable "aap_red_hat_username" {
+  type    = string
+  default = "aap_red_hat_username"
+}
+
+variable "aap_red_hat_password" {
+  type    = string
+  default = "aap_red_hat_password"
+}
